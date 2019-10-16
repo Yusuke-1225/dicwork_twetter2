a38691ea289402da3709363bdfadfb06788c0a26
yusuke1225 <chan.merey@icloud.com>
2019-10-16 18:32:29 +0900

    “画像機能追加”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_083041) do
+ActiveRecord::Schema.define(version: 2019_10_16_085917) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_10_15_083041) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
